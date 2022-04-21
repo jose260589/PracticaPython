7fccc3b41300d224594ae523dd59d5b63c35d91b
jose260589 <jose260589@yahoo.com>
2022-04-20 22:26:39 -0400

    Programa simple para mostrar tablas de multiplicar.

---
+++ Practicas/multiplicacion.py
@@ -0,0 +1,7 @@
+#Programa simple para presentar tabla de multiplicar.
+n = int(input("Digite el numero que desea multiplicar: "))
+
+print("--"*10+"/n")
+for i in range(1,20):
+    print(n," x ",i," = ",(n*i))
+print("--"*10+"/n")
