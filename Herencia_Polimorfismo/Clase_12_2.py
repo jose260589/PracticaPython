ef3be776c861df2ee315c258526ef9a940eacdec
jose260589 <jose260589@yahoo.com>
2023-08-26 23:15:45 -0400

    Guardar codigo clase12_2

--- Herencia_Polimorfismo/Clase_12_2.py
+++ Herencia_Polimorfismo/Clase_12_2.py
@@ -1,4 +1,25 @@
 #Polimorfismo
 
-# agregando linea de ejemplo
+class Animales:
+    def __init__(self,nombre):
+        self.nombre = nombre
+    
+    def tipo_animal(self):
+        pass
 
+class Leon(Animales):
+    def tipo_animal(self):
+        print('Animal salvaje')
+        
+class Perro(Animales):
+    def tipo_animal(self):
+        print('Animal domestico')
+        
+
+
+
+nuevo_animal = Leon("Simba")
+nuevo_animal.tipo_animal()
+
+nuevo_animal2 = Perro("Rex")
+nuevo_animal2.tipo_animal()
