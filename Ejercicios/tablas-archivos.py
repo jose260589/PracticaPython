d34b00c3ee7ac8580310642da79f3c8ab74ea02e
jose260589 <jose260589@yahoo.com>
2023-11-15 20:23:12 -0400

    Se crea un pograma que genera un archivo con la tabla de un numero introducido por el usuario.

---
+++ Ejercicios/tablas-archivos.py
@@ -0,0 +1,13 @@
+"""
+Crear un programa que genere un archivo con la tabla de un numero escrito por el usuario. del 1 al 10
+el numero puede ser aleatorio.
+"""
+
+
+multiplo = int(input("Digita el numero que se va a multiplicar: "))
+
+print(f"{multiplo}")
+
+
+
+
