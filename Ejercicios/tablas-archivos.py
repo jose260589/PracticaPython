0e2d113e9d8f5c51bf47a88acad5121e8dd935d6
jose260589 <jose260589@yahoo.com>
2023-11-15 20:24:25 -0400

    se termina programa que genera un archivo nuevo con la tabla de multiplicar segun el numero introducido por el usuario.

--- Ejercicios/tablas-archivos.py
+++ Ejercicios/tablas-archivos.py
@@ -2,12 +2,19 @@
 Crear un programa que genere un archivo con la tabla de un numero escrito por el usuario. del 1 al 10
 el numero puede ser aleatorio.
 """
+from io import open
+
+def archivar(numero):
+    multiplos = numero
+    newarchivo = open(f"tabla-{multiplos}.txt","a")
+    print(newarchivo.writable()) #Ver si tengo permiso de escritura
+    print(newarchivo.write(f"Tabla del {multiplos}\n"))
+    
+    for (i) in range(10): 
+        print(newarchivo.write(f"{multiplos} x {i+1} = {multiplos*(i+1)}\n"))
+    
+    newarchivo.close()
 
 
-multiplo = int(input("Digita el numero que se va a multiplicar: "))
-
-print(f"{multiplo}")
-
-
-
-
+multiplicar = int(input("Digita el numero que se va a multiplicar: "))
+archivar(multiplicar)
