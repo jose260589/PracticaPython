0ea7b2e2367c7a7e5abe64bcd43d016134f8efb6
jose260589 <jose260589@yahoo.com>
2025-05-05 23:37:59 -0400

    Other file data type.

---
+++ Tipos de datos/datatype.py
@@ -0,0 +1,34 @@
+#String
+print("Hello world")
+print('Hello World')
+print('''Hello world''')
+print("""Hello World""")
+
+#Interger
+print(10)
+
+#Float
+print(30.1)
+
+#Boelean
+False
+True
+
+#List
+#Contiene elementos, y los datos pueden cambiar
+[10,20,30,55]
+['Hello', 'Bye', "Aduis"]
+[10,"Hello", True, 10.1]
+
+
+#tuples o tuplas
+#no cambian los datos
+(10,20,30,55)
+
+#Diccionarios
+{"name": "Jose",
+ "lastname": "Inoa",
+ "nickname": "Ryo"}
+
+#Dato no definido
+None
