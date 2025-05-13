4ea98070be873308abbec79ee3930b540db2f231
jose260589 <jose260589@yahoo.com>
2025-05-12 22:16:51 -0400

    Prueba basica de numeros con input

---
+++ Tipos de datos/numbers.py
@@ -0,0 +1,16 @@
+#Enteros 9,10
+#Decimales o flotantes 5.5,3.3
+
+#type("numero o varialbe del numero") type(5) type(5.5) etc
+
+#operadores +,-,*,/
+"""operadores especiales
+    % operador de modula 3 % 2= 1
+    ** exponente, 2**3
+    // residuo 3//2
+"""
+
+ #Entrada desde consola con input, siempre devuelve un string, para usarlo como numero hay que cambiarle el tipo de dato.
+age = int(input("Inserta la edad: "))
+
+print(type(age))
