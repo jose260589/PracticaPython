875969dc26ed9ac92f25c60c9917cedf13d4c9dd
jose260589 <jose260589@yahoo.com>
2025-05-14 21:35:40 -0400

    agregando algunos metodos del tipo de dato set

---
+++ metodos/set.py
@@ -0,0 +1,16 @@
+#tipo de dato set, es un dato que no tiene un indice como son las listas y tuplas.
+
+Colors = {'Red', 'Green', 'Blue'}
+
+print(Colors)
+print('Red' in Colors) #Para validar si un valor esta en una lista, tuple, o dato set.
+Colors.add('Violet') #agrega un nuevo valor, pero a diferencia de la lista y tuples, este lo agrega al inicio
+print(Colors)
+
+Colors.remove('Red')
+print(Colors)
+
+#del colors # borra la variable.
+
+
+
