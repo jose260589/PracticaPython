21902bee03ccd0cbd454fa398f3a66e8efd071dc
jose260589 <jose260589@yahoo.com>
2025-05-14 21:24:21 -0400

    agregando algunos datos mas de las tuplas.

--- metodos/metodos_tuplas.py
+++ metodos/metodos_tuplas.py
@@ -1,5 +1,13 @@
     
     #es similar a la lista pero las tuplas usan (), las listas usan []
-    
+    # los datos no cambian, se pueden ejecutar mas rapido.
 
 tupla = ("jose")
+
+x = (1, 2, 3)
+
+#print(type(x))
+
+#dir podemos ver los elementos de una tuble o tupla
+
+print(dir(x))
