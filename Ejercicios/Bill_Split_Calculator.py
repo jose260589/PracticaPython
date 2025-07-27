42efa90a84952f12aaaf03f06616b4f9d0140da2
jose260589 <jose260589@yahoo.com>
2025-07-27 12:21:38 -0400

    nge

--- Ejercicios/Bill_Split_Calculator.py
+++ Ejercicios/Bill_Split_Calculator.py
@@ -1,10 +1,11 @@
 print("Bill Split Calculator")
 
 friend = int(input("How many friends are there? "))
-bill = int(input("How much was the bill?"))
-tax = bill * 0.10
+bill = int(input("How much was the bill?")) 
+tax = bill * 0.18 
+tip = bill * 0.10
 
-facture_Total = bill + tax
+facture_Total = bill + tax + tip
 
 friend_Total = facture_Total / friend
 
