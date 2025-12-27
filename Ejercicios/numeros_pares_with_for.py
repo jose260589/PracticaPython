d2d5c35837610466fc3e8a467c9b54121fe560f5
jose260589 <jose260589@yahoo.com>
2025-12-27 01:28:08 -0400

    Ejercicio Terminado.

--- Ejercicios/numeros_pares_with_for.py
+++ Ejercicios/numeros_pares_with_for.py
@@ -1,1 +1,8 @@
 # Generar un código que imprima los números pares del 10 al 50 utilizando un bucle for.
+count_even = 0
+
+for numero in range(10, 51):
+    if numero % 2 == 0:
+        count_even += 1
+        
+print(f"count_even = {count_even}")
