d111602ee009ff09beda088d6790ac9386b27ad2
jose260589 <jose260589@yahoo.com>
2025-12-27 15:05:30 -0400

    Agregando lineas con variables multiples.

--- Variables/variables.py
+++ Variables/variables.py
@@ -15,3 +15,9 @@ bienvenida = f"Hola {nombre_Completo_de_tu_tio_master} Como estas?"
 #Operadores de pertenecia (in / not in)
 print("Lucas " in bienvenida) #true
 print("Lucas " not in bienvenida) #false
+
+#Variables en una sola linea
+x, y, z = 1, 2, 3
+#Tambien pueden ser con datos diferentes. examplo:
+name,surname, age, height = "Jose", "Inoa", 36, 1.75
+print("Hola, me llamo:",name,surname,". Mi edad es:",age, "y mi altura es:",height)
