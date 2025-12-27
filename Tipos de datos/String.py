8020ace94240231f1bc602d04a1a7ab6c9b5347d
jose260589 <jose260589@yahoo.com>
2025-12-27 18:40:19 -0400

    Se elimina algunos espacios innecesarios.

--- Tipos de datos/String.py
+++ Tipos de datos/String.py
@@ -6,6 +6,7 @@ edad = 34
 #forma basica. 
 print("Hola Soy " + nombre) #una sola variable
 
+#forma anterior como se daba formato. "%s":String, "%d":Integer, "%f":Float, "%.number of digitsf": Float with fixed decimal places example %.2f 50.00
 #usando el % s
 print("Hola soy % s"%nombre)
 print("Hola soy % s y tengo % s años."%(nombre,edad))#si son mas de una variable hay que usar una tupla
@@ -14,7 +15,7 @@ print("Hola soy % s y tengo % s años."%(nombre,edad))#si son mas de una variabl
 radius = 10
 pi = 3.14
 area = pi * radius ** 2
-formated_string = 'The area of circle with a radius %d is %.3f.' %(radius, area)
+formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area)
 print(formated_string)
 
 #usando el metodo formart
@@ -22,4 +23,3 @@ print("Hola soy {} y tengo {} años.".format(nombre,edad))
 
 #usando el metodo f-strings(es el que suelo usar)
 print(f"Hola soy {nombre} y tengo {edad} años.")
-
