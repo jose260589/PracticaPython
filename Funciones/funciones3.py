7c8ea8982799976afd23f1a78b14ff12bf4a9d55
jose260589 <jose260589@yahoo.com>
2026-01-19 00:29:28 -0400

    funcion sumar dos numeros con retorno.

--- Funciones/funciones3.py
+++ Funciones/funciones3.py
@@ -1,1 +1,6 @@
 #crear funcion que retorne un valor solicitando dos numeros.
+def sumar_dos_numeros(num1, num2):  
+    return num1 + num2
+
+my_result = sumar_dos_numeros(5, 10)
+print("El resultado de la suma es:", my_result)
