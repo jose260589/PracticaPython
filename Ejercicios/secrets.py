345f5846616df5335b81001886ac3d7af60ff3ba
jose260589 <jose260589@yahoo.com>
2026-01-30 20:07:53 -0400

    codigo secreto.

---
+++ Ejercicios/secrets.py
@@ -0,0 +1,10 @@
+import secrets
+
+def generate_otp(length=6):
+    """Generates a secure one-time password (OTP) of specified length."""
+    return ''.join(str(secrets.randbelow(10)) 
+                   for _ in range(length))
+
+print("Generated OTP:", generate_otp())
+
+#source code --clcoding.comn
