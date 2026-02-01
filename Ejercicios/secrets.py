60b02fda791f136b3262ab4d830879b51a23500c
jose260589 <jose260589@yahoo.com>
2026-02-01 17:20:04 -0400

    revision archivo secrets.

--- Ejercicios/secrets.py
+++ Ejercicios/secrets.py
@@ -2,8 +2,7 @@ import secrets
 
 def generate_otp(length=6):
     """Generates a secure one-time password (OTP) of specified length."""
-    return ''.join(str(secrets.randbelow(10)) 
-                   for _ in range(length))
+    return ''.join(str(secrets.randbelow(10)) for _ in range(length))
 
 print("Generated OTP:", generate_otp())
 
