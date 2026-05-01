306bdf57d225c8410eca81970acd1326d571d7ab
jose260589 <jose260589@yahoo.com>
2026-04-30 22:09:03 -0400

    se modifico el archivo suma.py, y se trato de modificar las carpetas Excepciones y PracticaPytyhon

---
+++ suma.py
@@ -0,0 +1,11 @@
+# Programa para sumar dos números
+
+# Solicitar los números al usuario
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Realizar la suma
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print(f"La suma de {numero1} + {numero2} = {resultado}")
